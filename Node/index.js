8534b26f2da300b5a4ca6dac6e738fc7dd346852
Rodrigo de Lourdes Silva <rodrigo.lourdes20@gmail.com>
2022-09-25 18:56:41 -0300

    Create index.js

---
+++ Node/index.js
@@ -0,0 +1,23 @@
+const questions = [
+    "O que aprendi hoje?",
+    "O que me deixou aborrecido?",
+    "O que eu poderia fazer para melhorar?",
+    "O que me deixou feliz hoje",
+    "Quantas pessoas ajudei hoje",
+]
+
+const ask = (index = 0) => {
+    process.stdout.write("/n" + questions[index] + " > ")
+}
+
+ask()
+
+const answers = []
+process.stdin.on("data", data => {
+    process.stdout.write( data.toString().trim())
+    if (answers.length < questions.length) {
+        ask(answers.length)
+    } else {
+        process.exit()
+    }
+})
