e3a3a24ece32a00ae6f21e5026b2329c87794f51
Rodrigo de Lourdes Silva <rodrigo.lourdes20@gmail.com>
2022-09-25 19:06:31 -0300

    NV

--- Node/index.js
+++ Node/index.js
@@ -21,3 +21,22 @@ process.stdin.on("data", data => {
         process.exit()
     }
 })
+
+process.on('exit', () => {
+    console.log(`
+    Bacana Rodrigo!
+
+    O que você aprendeu hoje foi:
+    ${answers[0]}
+
+    O que te aborreceu e você poderia melhorar foi:
+    ${answers[1]}
+
+    O que te deixou feliz hoje:
+    ${answers[2]}
+
+    Você ajudou ${answers[3]} pessoas hoje!!
+
+    Volte amanhã para nvoas reflexões!!
+    `)
+})
